92cd2dcb1848425a895d7987a846eeb24f062a71
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-13 13:17:59 +0700

    Install custom exception handlers

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import Path
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from fastapi import status
 
 from typing import Annotated
+
+from exceptions.uniconrn_exception import UnicornException
 
 app = FastAPI()
 
@@ -29,3 +34,20 @@ async def read_item(id: Annotated[str, Path()]):
     )
 
   return { "item": ITEMS[id] }
+
+# Install custom exception handlers
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exception: UnicornException):
+  return JSONResponse(
+    status_code = status.HTTP_418_IM_A_TEAPOT,
+    content = {
+      "message": f"Oops! {exception.name} did something. There goes a rainbow..."
+    }
+  )
+
+@app.get("/unicorns/{name}")
+async def read_unicorns(name: Annotated[str, Path()]):
+  if name == "yolo":
+    raise UnicornException(name = name)
+
+  return {"name": name}
