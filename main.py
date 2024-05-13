3479fdc100db8ecb9495a65c67952a618b32818e
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-13 13:55:31 +0700

    Use the RequestValidationError body

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import Path
+from fastapi import Body
 from fastapi import Request
 from fastapi import status
+
 from fastapi.responses import JSONResponse
 from fastapi.responses import PlainTextResponse
-from fastapi.exceptions import RequestValidationError, StarletteHTTPException
+
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from typing import Annotated
 
 from exceptions.uniconrn_exception import UnicornException
+
+from models.item import Item
 
 app = FastAPI(debug = True)
 
@@ -57,9 +64,11 @@ async def read_unicorns(name: Annotated[str, Path()]):
   return {"name": name}
 
 # Override the default exception handlers
+"""
 @app.exception_handler(RequestValidationError)
 async def request_validation_error_handler(request: Request, exception: RequestValidationError):
   return PlainTextResponse(str(exception), status_code = status.HTTP_400_BAD_REQUEST)
+"""
 
 @app.exception_handler(StarletteHTTPException)
 async def starlette_http_Exception_handler(request: Request, exception: StarletteHTTPException):
@@ -74,3 +83,18 @@ async def read_item(id: Annotated[int, Path()]):
     )
 
   return {"id": id}
+
+# Use the RequestValidationError body
+@app.exception_handler(RequestValidationError)
+async def request_validation_error_handler(request: Request, exception: RequestValidationError):
+  return JSONResponse(
+    status_code = status.HTTP_422_UNPROCESSABLE_ENTITY,
+    content = jsonable_encoder({
+      "errors": exception.errors(),
+      "body": exception.body
+    })
+  )
+
+@app.post("/items", status_code = status.HTTP_201_CREATED)
+async def create_item(item: Annotated[Item, Body(embed = True)]):
+  return item
