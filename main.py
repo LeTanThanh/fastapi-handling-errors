1c632631866dfd0ed9100878e08eecd322cabf35
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-13 12:04:56 +0700

    Add custom headers

--- main.py
+++ main.py
@@ -6,12 +6,26 @@ from typing import Annotated
 
 app = FastAPI()
 
+ITEMS = { "foo": "The Foo Wrestlers" }
+
 # Use HTTPException
+"""
 @app.get("/items/{id}")
 async def read_item(id: Annotated[str, Path()]):
-  items = { "foo": "The Foo Wrestlers" }
-
-  if id not in items:
+  if id not in ITEMS:
     raise HTTPException(status_code = 404, detail = "Item not found")
 
-  return { "item": items[id] }
+  return { "item": ITEMS[id] }
+"""
+
+# Add custom headers
+@app.get("/items/{id}")
+async def read_item(id: Annotated[str, Path()]):
+  if id not in ITEMS:
+    raise HTTPException(
+      status_code = 404,
+      detail = "Item not found",
+      headers = {"X-Error": "There goes my error"}
+    )
+
+  return { "item": ITEMS[id] }
